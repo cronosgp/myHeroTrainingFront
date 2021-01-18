dc521bf47865b2d9b0b44c7ac66aa3f895e20c6e
rpitta <raphael@outroraca.com.br>
2021-01-18 14:28:15 -0300

    teste

--- public/js/services/homeService.js
+++ public/js/services/homeService.js
@@ -3,7 +3,7 @@ angular.module('myHeroTraining').factory('myHeroTraining', function ($http) {
     var jwt = localStorage.getItem('Bearer');
 
     $http.defaults.headers.common.Authorization = 'Bearer ' + jwt;
-    return $http.get('http://localhost:8080/treinos', {
+    return $http.get('https://mhtrainingback.herokuapp.com/treinos', {
       params: {
         id: id,
       },
@@ -14,7 +14,7 @@ angular.module('myHeroTraining').factory('myHeroTraining', function ($http) {
     var jwt = localStorage.getItem('Bearer');
 
     $http.defaults.headers.common.Authorization = 'Bearer ' + jwt;
-    return $http.get('http://localhost:8080/tempo', {
+    return $http.get('https://mhtrainingback.herokuapp.com/tempo', {
       params: {
         id_usuario: id_usuario,
         id_fase: id_fase,
