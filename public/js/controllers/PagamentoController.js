6ce88c276b3946d0ea4253e890ede27b3587ae5c
larissaCarvalho23 <carvalholb95@hotmail.com>
2021-03-17 10:49:52 -0300

    teste pagmento

--- public/js/controllers/PagamentoController.js
+++ public/js/controllers/PagamentoController.js
@@ -5,6 +5,34 @@ angular
     
     function ($location) {
         
+        document.getElementById('docNumber').addEventListener('change', ()=>{
+            document.getElementById('docNumber').style.border='';
+            document.getElementById('docNumber_span').style.color='';
+            document.getElementById('docNumber_span').innerText="";
+        })
+
+        document.getElementById('cardNumber').addEventListener('change', ()=>{
+            document.getElementById('cardNumber').style.border='';
+            document.getElementById('cardNumber_span').style.color='';
+            document.getElementById('cardNumber_span').innerText="";
+        })
+        
+        document.getElementById('securityCode').addEventListener('change', ()=>{
+            document.getElementById('securityCode').style.border='';
+            document.getElementById('securityCode_span').style.color='';
+            document.getElementById('securityCode_span').innerText="";
+        })
+        document.getElementById('cardExpirationYear').addEventListener('change', ()=>{
+            document.getElementById('cardExpirationYear').style.border='';
+            document.getElementById('cardExpirationYear_span').style.color='';
+            document.getElementById('cardExpirationYear_span').innerText="";
+        })
+        document.getElementById('cardExpirationMonth').addEventListener('change', ()=>{
+            document.getElementById('cardExpirationMonth').style.border='';
+            document.getElementById('cardExpirationMonth_span').style.color='';
+            document.getElementById('cardExpirationMonth_span').innerText="";
+        })
+        
         
  //REPLACE WITH YOUR PUBLIC KEY AVAILABLE IN: https://developers.mercadopago.com/panel/credentials
  window.Mercadopago.setPublishableKey("TEST-95e84925-5fd8-44b9-9e1d-d4e92d7d2f8b");
@@ -53,12 +81,7 @@ angular
          }
  
      } else {
-            Swal.fire({
-            icon: 'error',
-            title: 'Oops...',
-            text: 'Problemas com seu método de pagamento, verifique os dados inseridos',
-         
-          })
+         alert(`payment method info error: ${response}`);
      }
  }
  
@@ -93,7 +116,7 @@ angular
          );
  
      } else {
-           Swal.fire({
+        Swal.fire({
             icon: 'error',
             title: 'Oops...',
             text: 'Método de pagamento inválido',
@@ -192,7 +215,7 @@ angular
          
          http.onreadystatechange = function() {//Call a function when the state changes.
              if(http.readyState == 4 && http.status == 200) {
-                 if(http.response.includes('Approved')){
+                 if(!http.responseText.includes('rejected')){
                 Swal.fire({
                     title: 'Seu pagamento foi aprovado com sucesso',
                     icon: 'success',
@@ -240,15 +263,54 @@ angular
 
 
      } else {
-   Swal.fire({
+var response_error;
+        console.log(response.cause)
+for(var i=0; i<response.cause.length; i++){
+response_error +=" "+ response.cause[i].description
+
+if(response.cause[i].description.includes('docNumber') ){
+
+    document.getElementById('docNumber').style.border='1px solid red'
+    document.getElementById('docNumber_span').style.color='red'
+    document.getElementById('docNumber_span').innerText= 'Campo número documento inválido'
+}
+  if(response.cause[i].description.includes('cardNumber')){
+ 
+      document.getElementById('cardNumber').style.border='1px solid red'
+      document.getElementById('cardNumber_span').style.color='red'
+     document.getElementById('cardNumber_span').innerText= 'Número de cartão inválido'
+
+  }
+  if(response.cause[i].description.includes('securityCode')){
+    
+      document.getElementById('securityCode').style.border='1px solid red'
+      document.getElementById('securityCode_span').style.color='red'
+     document.getElementById('securityCode_span').innerText= 'Código de segurança inválido'
+
+  }
+  if(response.cause[i].description.includes('cardExpirationMonth')){
+
+    document.getElementById('cardExpirationMonth').style.border='1px solid red'
+    document.getElementById('cardExpirationMonth_span').style.color='red'
+   document.getElementById('cardExpirationMonth_span').innerText= 'Mês de validade inválido'
+  }
+  if(response.cause[i].description.includes('cardExpirationYear')){
+    document.getElementById('cardExpirationYear').style.border='1px solid red'
+ 
+    document.getElementById('cardExpirationYear_span').style.color='red'
+   document.getElementById('cardExpirationYear_span').innerText= 'Ano de validade inválido'
+  }
+}
+        Swal.fire({
             icon: 'error',
             title: 'Oops...',
-            text: 'Verifique os dados inseridos no formulário',
+            text: "Problemas ao processar, valide suas informações.",
          
-          })     }
+          })
+     }
  };
  
- /***
+ /*
   * UX functions 
   */
  function PaytoBoleto(){
@@ -329,12 +391,6 @@ Swal.fire({
  document.getElementById('description').value = document.getElementById('product-description').innerHTML;
 
  
+
  
 });
- 
-
-
-
-
-
-
