1dadc17d36b53271a2010673160d7790d380efb6
Gabriel Gomes Pereira da Silva <gabriel.gomes@ibm.com>
2021-03-19 00:36:52 -0300

    treino-conjunto

--- public/js/controllers/TreinoConjuntoController.js
+++ public/js/controllers/TreinoConjuntoController.js
@@ -136,7 +136,6 @@ angular
                             type: "error",
                             icon: "error"
                         })
-                        $location.path('/treino-conjunto/solicitacoes');
                     }
                 })
             })
