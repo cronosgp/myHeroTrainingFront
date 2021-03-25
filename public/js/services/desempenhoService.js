efa027d8f961998c888b9dd551d6999ad714f918
raphaelpitta1 <raphael.pitta@hotmail.com>
2021-03-25 00:35:24 -0300

    ajustes_graficos

--- public/js/services/desempenhoService.js
+++ public/js/services/desempenhoService.js
@@ -1,33 +1,31 @@
 angular.module('myHeroTraining').factory('desempenhoService', function ($http) {
-    var busca = function (id, data, dataf, filtro) {
- //    var jwt = localStorage.getItem('Bearer');
-  
-   //  $http.defaults.headers.common.Authorization = 'Bearer ' + jwt;
-      return $http.get('https://mhtrainingback.herokuapp.com/desempenho', {
-        params: {
-          id: id,
-         d_ini : data ,
-          dt_fim : dataf ,
-          filtro: filtro
-        },
-      });
+  var busca = function (id, data, dataf, filtro) {
+//    var jwt = localStorage.getItem('Bearer');
+
+ //  $http.defaults.headers.common.Authorization = 'Bearer ' + jwt;
+    return $http.get('https://mhtrainingback.herokuapp.com/desempenho', {
+      params: {
+        id: id,
+       d_ini : data ,
+        dt_fim : dataf ,
+        filtro: filtro
+      },
+    });
+  };
+  var carregaDados = function(id){
+    //var jwt = localStorage.getItem('Bearer');
+
+   // $http.defaults.headers.common.Authorization = 'Bearer ' + jwt;
+    return $http.get('https://mhtrainingback.herokuapp.com/desempenho/dado', {
+      params: {
+        id: id,
       
-    };
-    var carregaDados = function(id){
-      //var jwt = localStorage.getItem('Bearer');
-  
-     // $http.defaults.headers.common.Authorization = 'Bearer ' + jwt;
-      return $http.get('https://mhtrainingback.herokuapp.com/desempenho/dado', {
-        params: {
-          id: id,
-        
-    }
-  });
+  }
+});
 }
-       return {
-        busca: busca,
-        carregaDados : carregaDados
-       
-      };
-  });
-  
+     return {
+      busca: busca,
+      carregaDados : carregaDados
+     
+    };
+});
